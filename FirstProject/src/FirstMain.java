25810bc8ef9d63abb066c215f8c691834515065f
kit-lec <kit_lec@hotmail.com>
2023-04-11 23:47:56 +0900

    230411

---
+++ FirstProject/src/FirstMain.java
@@ -0,0 +1,9 @@
+public class FirstMain {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println(100 * 34);
+        System.out.println("제 코드 보이나요?");
+    }
+
+}
