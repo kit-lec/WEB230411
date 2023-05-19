e5f995d24413e502a8a52a6033c52fddc509c59d
kit-lec <kit_lec@hotmail.com>
2023-05-19 23:22:28 +0900

    230519

--- WebWork/03_JavaScript/C_Web/JS/myScript.js
+++ WebWork/03_JavaScript/C_Web/JS/myScript.js
@@ -1,3 +1,18 @@
 /**
  * TODO
  */
+
+function light(sw){
+    let pic;
+    if(sw == 0){
+        pic = "https://www.w3schools.com/html/pic_bulboff.gif"
+    } else {
+        pic = "https://www.w3schools.com/html/pic_bulbon.gif"
+    }
+
+    // element개체.attribute이름
+    document.getElementById('myImage').src = pic;
+    // id 값 'myImage' 를 가진 element 를 찾아서 
+    // 'src' attribute 값을 변경
+
+}
